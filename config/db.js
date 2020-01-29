8a9b280b938164aad3bc88605bc73a6a57014a19
Muhammad Aijaz <muhammadaijaz76@gmail.com>
2020-01-29 14:32:09 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://name:password@cluster0-ywqrk.mongodb.net/<databaseName>?retryWrites=true&w=majority"
+const mongoURI = "uri"
 
 mongoose.connect(mongoURI);
 
