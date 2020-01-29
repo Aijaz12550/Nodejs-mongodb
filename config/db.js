62862aed114bab3e39d0b769ca2ca3fc2e351f6f
Muhammad Aijaz <muhammadaijaz76@gmail.com>
2020-01-29 14:33:30 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://aijaz:aijaz@cluster0-ywqrk.mongodb.net/campusRS?retryWrites=true&w=majority"
+const mongoURI = "urui"
 
 mongoose.connect(mongoURI);
 
